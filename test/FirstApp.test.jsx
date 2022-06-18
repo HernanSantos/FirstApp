397f63dc17cdd4fec25f7d9c3c8dbaf0363342f5
HernanSantos <xsantoshernan@gmail.com>
2022-06-17 23:51:29 -0300

    nuevos cambios

--- test/FirstApp.test.jsx
+++ test/FirstApp.test.jsx
@@ -4,7 +4,19 @@ import { FirstApp } from "../src/FirstApp";
 describe('prueba en FirstApp', () => {
     test('debe de hacer match', () => {
         
-        render(<FirstApp title = "soy un titulo"/>)
+        const title = "Hola, soy Goku";
+        const {container,getByText} = render(<FirstApp title= {title}/>);
+
+        expect(container).toMatchSnapshot();
+    });
+
+    test('debe mostrar el titulo en un h1', () => {
+        
+        const title = "Hola, soy Goku";
+        const {container,getByText} = render(<FirstApp title= {title}/>);
+
+        const h1 = container.querySelector('h1');
+        expect(h1.innerHTML).toContain(title);
 
     });
 })
